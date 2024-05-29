e80e86c47b2f9c7595f178a39bd082df456a87d5
Ronaldo-Junior1 <ronaldosjr2005@gmail.com>
2024-05-29 16:33:40 -0300

    feat: Implementação base da classe calculadora e seus metodos.

--- NewTalents/Models/Calculadora.cs
+++ NewTalents/Models/Calculadora.cs
@@ -8,6 +8,30 @@ namespace NewTalents.Models
 {
     public class Calculadora
     {
+        public int Somar(int num1,int num2)
+        {
+            return 0;
+        }
+
+        public int Subtrair(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int Multiplicar(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int Dividir(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public List<string> Historico()
+        {
+            return new List<string>();
+        }
 
     }
 }
